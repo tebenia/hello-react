74904af94f7692b08615dbfdcd12309b40644da3
Tribudi Sabdo Utomo <tebe.deye@gmail.com>
2017-07-29 04:41:33 +0700

    4. Learn "props" by changing color

--- src/App.js
+++ src/App.js
@@ -2,30 +2,60 @@ import React from 'react';
 import createReactClass from 'create-react-class';
 import './App.css';
 
-let Timer = createReactClass({
+let ColorDisplay = createReactClass({
+  render: function() {
+    const displayStyle = {backgroundColor: "#"+ this.props.bgCol};
+    return (
+      <h1 style={displayStyle}>Ganti warna latar</h1>
+    );
+  }
+})
+
+let PresetButton = createReactClass({
+  handleClick: function(event){
+    this.props.onColorSelected(this.props.preset);
+  },
+  render: function() {
+    const displayStyle = {backgroundColor: "#" + this.props.preset};
+    return (
+      <button onClick={this.handleClick}><i style={displayStyle}></i> {this.props.preset}</button>
+    );
+  }
+})
+
+let TextInput = createReactClass({
+  handleTextInput: function(event){
+    this.props.onColorSelected(event.target.value);
+  },
+  render: function() {
+    return (
+      <input type="text" onChange={this.handleTextInput} value={this.props.currentColor}/>
+    );
+  }
+})
+
+let App = createReactClass({
   getInitialState: function() {
     return {
-      secElapsed:0
+      color:"ccc"
     };
   },
-  componentDidMount: function(){
-    this.interval = setInterval(this.tick, 1000);
-  },
-  componentWillUnmount: function(){
-    clearInterval(this.interval);
-  },
-  tick: function(){
+  handleColorSection: function(selectedColor){
     this.setState({
-      secElapsed: this.state.secElapsed + 1
+      color:selectedColor
     });
   },
   render: function(){
     return (
       <div>
-        <p>Detik terlewati {this.state.secElapsed}</p>
+        <ColorDisplay bgCol={this.state.color}/>
+        <PresetButton preset="c00" onColorSelected={this.handleColorSection}/>
+        <PresetButton preset="0c0" onColorSelected={this.handleColorSection}/>
+        <PresetButton preset="00c" onColorSelected={this.handleColorSection}/>
+        <TextInput onColorSelected={this.handleColorSection} currentColor={this.state.color}/>
       </div>
     )
   }
 })
 
-export default Timer;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Timer from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Timer />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
